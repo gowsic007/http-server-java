1fb5bdc1828ba6040daa48bfe171f1695f2cd282
Gowsic N <gowsic.n@booking.com>
2025-08-18 17:09:50 +0530

    feat : Add error response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.Builder;
 
 public class Main {
 
@@ -21,12 +25,12 @@ public class Main {
             System.out.println("Accepted new connection");
 
             processRequestResponse(socket);
-        } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
+        } catch (IOException | URISyntaxException e) {
+            System.out.println("Failed to Process request : " + e.getMessage());
         }
     }
 
-    private static void processRequestResponse(Socket socket) throws IOException {
+    private static void processRequestResponse(Socket socket) throws IOException, URISyntaxException {
         BufferedReader inReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
         OutputStream outWriter = socket.getOutputStream();
@@ -34,11 +38,52 @@ public class Main {
         // Reading the request
         String requestMessage = inReader.readLine();
         System.out.println("Request message: " + requestMessage);
+        HttpRequest request = parseRequest(socket, requestMessage);
 
         // Writing the response
-        String responseMessage = "HTTP/1.1 200 OK\r\n\r\n";
+        String responseMessage = "";
+        if (request.uri().getPath().isEmpty() || request.uri().getPath().equals("/index.html")) {
+            responseMessage = "HTTP/1.1 200 OK\r\n\r\n";
+        } else {
+            responseMessage = "HTTP/1.1 404 Not Found\r\n\r\n";
+        }
         outWriter.write(responseMessage.getBytes());
         outWriter.flush();
         System.out.println("Response returned: " + responseMessage);
     }
+
+    private static HttpRequest parseRequest(Socket socket, String requestMessage) throws URISyntaxException {
+        Builder httpRequestBuilder = HttpRequest.newBuilder();
+        String[] requestArray = requestMessage.split("\r\n");
+        String[] hostDetailsArray =  requestArray[0].split(" ");
+        String methodName = hostDetailsArray[0];
+        String path = hostDetailsArray[1];
+
+        switch (methodName) {
+            case "GET":
+                httpRequestBuilder.GET();
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown method name: " + methodName);
+        }
+
+        URI uri = new URI("http", null, socket.getInetAddress().getHostAddress(), -1, path, null, null);
+        httpRequestBuilder.uri(uri);
+
+        String httpVersion = hostDetailsArray[2];
+        switch (httpVersion) {
+            case "HTTP/1.1":
+                httpRequestBuilder.version(Version.HTTP_1_1);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown Http version: " + httpVersion);
+        }
+
+        for(int i = 1; i < requestArray.length; i++) {
+            String[] headerKV = requestArray[i].split(": ");
+            httpRequestBuilder.setHeader(headerKV[0], headerKV[1]);
+        }
+
+        return httpRequestBuilder.build();
+        }
 }
